5f95a0cdf1a5ff7fdf03a38a2d8915def236c116
ngonamduonghl <duongnamngohl@gmail.com>
2023-02-14 20:57:52 +0700

    update authentication

--- components/auc.py
+++
@@ -1,6 +0,0 @@
-import random
-from algorithm.com128 import auth
-def generate_triples(Ki):
-    RAND = random(0, 65535)
-    Kc, SRES = auth(Ki, RAND)
-    return RAND, Kc, SRES
--- components/phone.py
+++ components/phone.py
@@ -1,16 +1,29 @@
+from algorithm.com128 import auth
 
 class Phone:
     def __init__(self, number, name):
         self.number = number
         self.name = name
         self.bts = None
+        self.rand = None
+        self.Ki = None
 
     def connect_to_bts(self, bts):
         if bts.add_phone(self):
             self.bts = bts
+            self.bts.bsc.msc.vlr.white.append(self)
+
+    def cal_SRES(self, RAND):
+        if(self.rand == None):
+            self.rand = RAND
+        SRES, Kc = auth(self.Ki, RAND)
+        return SRES
 
     def call(self, number):
-        self.bts.bsc.make_call(self, number)
+        if(self.bts.bsc.msc.vlr.auth_check(self)):
+            self.bts.bsc.make_call(self, number)
+            return True
+        return False
 
     def text(self, number, message):
         self.bts.bsc.send_sms(self, number, message)
